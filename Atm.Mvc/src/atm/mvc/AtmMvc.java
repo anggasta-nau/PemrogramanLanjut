9e2271a990f03c73199891c1581e86813517d742
Anggasta Naufal Akma <anggastanaufal@gmail.com>
2022-10-02 09:11:09 +0700

    Add files via upload

---
+++ Atm.Mvc/src/atm/mvc/AtmMvc.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atm.mvc;
+
+import Controller.AtmController;
+
+
+
+/**
+ *
+ * @author Anggasta
+ */
+public class AtmMvc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        AtmController  cek = new AtmController();
+    }
+    
+}
